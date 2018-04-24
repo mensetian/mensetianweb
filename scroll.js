6961b052abf67a2b3a19119c2c802162c23b78b6
sebas <sebas@debian>
2018-04-24 08:37:30 -0500

    newtwo121

--- scroll.js
+++ scroll.js
@@ -1,24 +1,5 @@
-var last_known_scroll_position = 0;
-var ticking = false;
-
-function doSomething(scroll_pos) {
-  // do something with the scroll position
-  alert('entro');
-}
-
-window.addEventListener('scroll', function(e) {
-
-  last_known_scroll_position = window.scrollY;
-
-  if (!ticking) {
-
-    window.requestAnimationFrame(function() {
-      doSomething(last_known_scroll_position);
-      ticking = false;
-    });
-     
-    ticking = true;
-
-  }
-  
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth' 
 });
