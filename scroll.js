fbe889ed0300902f6b212a46caa37c96d7e28fe0
sebas <sebas@debian>
2018-04-24 15:56:32 -0500

    new2

--- scroll.js
+++ scroll.js
@@ -1,12 +1,10 @@
 
 
-$('body').bind('mousewheel', function(e){
-    console.log('entro');
-    
-    if(e.originalEvent.wheelDelta /120 > 0) {
+$('body').bind('mousewheel', function (e) {
+    console.log(e.originalEvent.wheelDelta);
+    if (e.originalEvent.wheelDelta / 120 > 0) {
         alert('up');
-    }
-    else{
+    } else {
         alert('down');
     }
 });
