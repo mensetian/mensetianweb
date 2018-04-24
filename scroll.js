dc9183e343aacb6dd11e326d290c6a1962f94db9
sebas <sebas@debian>
2018-04-24 16:26:36 -0500

    new2

--- scroll.js
+++ scroll.js
@@ -2,7 +2,10 @@
 
 $('body').bind('mousewheel', function (e) {
     console.log(e.originalEvent.wheelDelta);
-    $('body span').addClass('animado').removeClass('animado');
+        $('body span').addClass('animado');
+    setTimeout(function () {
+        $('body span').removeClass('animado');
+    }, 300);
     if (e.originalEvent.wheelDelta / 120 > 0) {
         console.log('up');
     } else {
